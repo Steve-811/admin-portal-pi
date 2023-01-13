36ff73f4c314fd7e2c3fd3dd76f162847bfa5e9c
Hazik <gdsteve11@gmail.com>
2023-01-13 23:39:56 +0400

    changes

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express       = require('express')
 const app           = express()
 const bodyParser    = require('body-parser')
 
+app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 const { students,studentsList } = require('./students')
 
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  })
+  
 app.get('/students', studentsList)
 
 app.post('/students/create', students)
